bad28fdcc9d64a6c82aae0455003b20f8e9cea31
Kasey Fillpot <42328226+kfillpot@users.noreply.github.com>
2019-01-11 17:14:06 -0800

    Add files via upload

---
+++ bamazon.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS bamazon_db;
+
+CREATE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products(
+	item_id INTEGER(10) AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(15),
+    department_name VARCHAR(15),
+    price INTEGER(10),
+    stock_quantity INTEGER(10),
+    PRIMARY KEY(item_id)
+);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Dell Laptop", "Electronics", 750.00, 25),
+("OnePlus 6", "Electronics", 550.00, 3),
+("Nintendo Switch", "Electronics", 250.00, 12),
+("Record Player", "Electronics", 124.99, 100),
+("The Beach Boys", "Music", 19.99, 35),
+("Black Jeans", "Clothing", 15.99, 50),
+("Slip N' Slide", "Outdoors", 34.99, 20),
+("Rechargable Batteries", "Electronics", 19.99, 60);
+
+
+SELECT * FROM products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,96 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var customerRequest = [];
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon"
+});
+
+function bamazonProduct (){
+    connection.query("SELECT * FROM products", function (err,res){
+        if (err) throw err;
+
+        console.log("--------------------")
+        console.log("Bamazon Products: ");
+        console.log("--------------------")
+        for(var i = 0; i < res.length; i++) {
+            console.log("PRODUCT ID " + res[i].item_id + "PRODUCT " + res[i].product_name + 
+            "PRODUCT PRICE " + res[i].price);
+            customerRequest.push(res[i].product_name);
+        }
+
+        console.log("--------------------");
+        userPurchase();
+    });
+}
+
+function userPurchase(){
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to purchase today?",
+            choices: productList,
+            name: "product"
+          },
+          {
+            type: "input",
+            message: "Enter Quantity: ",
+            name: "quantity"
+          }
+        ]).then(function (inquirerResponse) {
+          var item = inquirerResponse.product;
+          var prodQuery = "SELECT * FROM products WHERE product_name='" + item + "' LIMIT 1";
+          var custAmt = parseInt(inquirerResponse.quantity);
+    
+          connection.query(prodQuery, function (err, res) {
+            if (err) throw err;
+    
+            var stock = res[0].stock_quantity;
+            var price = res[0].price;
+            var total = custAmt*price;
+            var bamSales = res[0].product_sales;
+    
+            if (stock > custAmt) {
+              productUpdate(item, stock, custAmt, total, bamSales);
+            }
+            else {
+              console.log("Insufficient quantity to Fulfill Order! Try Purchasing Different Amount");
+            }
+    
+          }); 
+          
+        }); 
+    }
+    
+    function productUpdate(name, x, y,z,s) {
+      var stockQty=x-y;
+      var prodSales=s+z;
+    
+      connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+          {
+            stock_quantity: stockQty,
+            product_sales: prodSales
+          },
+          {
+            product_name: name
+          }
+        ],
+        function (err, res) {
+          console.log("Thank you for Your Purchase! Your Total: $" + z);
+    
+          connection.end();
+        }
+      );
+    
+    }
+    
+    
+    bamazonProduct();
+
